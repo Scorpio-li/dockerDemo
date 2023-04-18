50a6d90385598b459082a073ca34b08ae9242425
lizhiliang <li.zhiliang@i-jimi.com.cn>
2023-04-18 10:02:33 +0800

    feat : 增加router配置

--- src/pages/errorException.vue
+++ src/pages/errorException.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+
 </script>
 
 <template>
-  <router-view></router-view>
+  <h1>项目错误处理</h1>
+  <img src='xxxxx' alt="错误图片">
 </template>
 
 <style scoped>
---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from '../pages/home.vue';
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/errorException',
+        name: 'ErrorException',
+        component: () => import('../pages/errorException.vue')
+    }
+]
+
+export const router = createRouter({
+    history: createWebHashHistory(),
+    routes: routes
+})
+
+export default router;
