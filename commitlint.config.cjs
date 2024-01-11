9dbd59a1de655f72f86ece07e0bd507925ab4bd1
lizhiliang <li.zhiliang@i-jimi.com.cn>
2024-01-11 18:33:15 +0800

    feat: 修改husky配置

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: Lizhiliang
  * @Date: 2024-01-11 18:18:45
- * @LastEditTime: 2024-01-11 18:19:06
+ * @LastEditTime: 2024-01-11 18:31:50
  * @LastEditors: lizhiliang
  * @Usage: 
  */
@@ -13,13 +13,17 @@ module.exports = {
             2,
             'always',
             [
-                'feature', // 迭代功能
+                'feat', // 迭代功能
                 'conf', // 修改构建配置
-                'fixbug', // 修复bug
+                'fix', // 修复bug
                 'refactor', // 代码重构
                 'optimize', // 代码优化
                 'style', // 仅修改样式文件
                 'docs', // 文档补充说明
+                'test', // 测试用例修改
+                'revert', // 代码回滚
+                'chore', // 其他修改, 比如改变构建流程、或者增加依赖库、工具等
+                'build', // 编译相关的修改，例如发布版本、对项目构建或者依赖的改动
             ],
         ],
         'header-max-length': [0, 'always', 72], //限制最长72
