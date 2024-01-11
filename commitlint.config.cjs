6270003fa6cdb6a3e55cbcc65d48e9014c36f95f
lizhiliang <li.zhiliang@i-jimi.com.cn>
2024-01-11 18:21:47 +0800

    feature: 提交测试

---
+++ commitlint.config.cjs
@@ -0,0 +1,27 @@
+/*
+ * @Description: 
+ * @Author: Lizhiliang
+ * @Date: 2024-01-11 18:18:45
+ * @LastEditTime: 2024-01-11 18:19:06
+ * @LastEditors: lizhiliang
+ * @Usage: 
+ */
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feature', // 迭代功能
+                'conf', // 修改构建配置
+                'fixbug', // 修复bug
+                'refactor', // 代码重构
+                'optimize', // 代码优化
+                'style', // 仅修改样式文件
+                'docs', // 文档补充说明
+            ],
+        ],
+        'header-max-length': [0, 'always', 72], //限制最长72
+    },
+};
